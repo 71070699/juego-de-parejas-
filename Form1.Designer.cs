dea2ea1037dae226bb71f022eaa7e2b5cb79f72f
Diego Antonio Rivera Lopez <usis054621@ugb.edu.sv>
2021-08-25 08:15:39 +0200

    Diego Rivera creo el ejercicio numero 6

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Juego_2_grupo3_parejas
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label16 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
@@ -46,6 +47,7 @@ namespace Juego_2_grupo3_parejas
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -77,14 +79,13 @@ namespace Juego_2_grupo3_parejas
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(712, 629);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 511);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label16
@@ -92,10 +93,9 @@ namespace Juego_2_grupo3_parejas
             this.label16.AutoSize = true;
             this.label16.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label16.ForeColor = System.Drawing.Color.Black;
-            this.label16.Location = new System.Drawing.Point(537, 470);
-            this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label16.Location = new System.Drawing.Point(404, 383);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(169, 157);
+            this.label16.Size = new System.Drawing.Size(125, 126);
             this.label16.TabIndex = 16;
             this.label16.Text = "c";
             this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -106,10 +106,9 @@ namespace Juego_2_grupo3_parejas
             this.label15.AutoSize = true;
             this.label15.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label15.ForeColor = System.Drawing.Color.Black;
-            this.label15.Location = new System.Drawing.Point(360, 470);
-            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Location = new System.Drawing.Point(271, 383);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(167, 157);
+            this.label15.Size = new System.Drawing.Size(125, 126);
             this.label15.TabIndex = 15;
             this.label15.Text = "c";
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -120,10 +119,9 @@ namespace Juego_2_grupo3_parejas
             this.label14.AutoSize = true;
             this.label14.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label14.ForeColor = System.Drawing.Color.Black;
-            this.label14.Location = new System.Drawing.Point(183, 470);
-            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Location = new System.Drawing.Point(138, 383);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(167, 157);
+            this.label14.Size = new System.Drawing.Size(125, 126);
             this.label14.TabIndex = 14;
             this.label14.Text = "c";
             this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -134,10 +132,9 @@ namespace Juego_2_grupo3_parejas
             this.label13.AutoSize = true;
             this.label13.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label13.ForeColor = System.Drawing.Color.Black;
-            this.label13.Location = new System.Drawing.Point(6, 470);
-            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Location = new System.Drawing.Point(5, 383);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(167, 157);
+            this.label13.Size = new System.Drawing.Size(125, 126);
             this.label13.TabIndex = 13;
             this.label13.Text = "c";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -148,10 +145,9 @@ namespace Juego_2_grupo3_parejas
             this.label12.AutoSize = true;
             this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label12.ForeColor = System.Drawing.Color.Black;
-            this.label12.Location = new System.Drawing.Point(537, 314);
-            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Location = new System.Drawing.Point(404, 256);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(169, 154);
+            this.label12.Size = new System.Drawing.Size(125, 125);
             this.label12.TabIndex = 12;
             this.label12.Text = "c";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -162,10 +158,9 @@ namespace Juego_2_grupo3_parejas
             this.label11.AutoSize = true;
             this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label11.ForeColor = System.Drawing.Color.Black;
-            this.label11.Location = new System.Drawing.Point(360, 314);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(271, 256);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(167, 154);
+            this.label11.Size = new System.Drawing.Size(125, 125);
             this.label11.TabIndex = 11;
             this.label11.Text = "c";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -176,10 +171,9 @@ namespace Juego_2_grupo3_parejas
             this.label10.AutoSize = true;
             this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label10.ForeColor = System.Drawing.Color.Black;
-            this.label10.Location = new System.Drawing.Point(183, 314);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Location = new System.Drawing.Point(138, 256);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(167, 154);
+            this.label10.Size = new System.Drawing.Size(125, 125);
             this.label10.TabIndex = 10;
             this.label10.Text = "c";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -190,10 +184,9 @@ namespace Juego_2_grupo3_parejas
             this.label9.AutoSize = true;
             this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(6, 314);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(5, 256);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(167, 154);
+            this.label9.Size = new System.Drawing.Size(125, 125);
             this.label9.TabIndex = 9;
             this.label9.Text = "c";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -204,10 +197,9 @@ namespace Juego_2_grupo3_parejas
             this.label8.AutoSize = true;
             this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label8.ForeColor = System.Drawing.Color.Black;
-            this.label8.Location = new System.Drawing.Point(537, 158);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(404, 129);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(169, 154);
+            this.label8.Size = new System.Drawing.Size(125, 125);
             this.label8.TabIndex = 8;
             this.label8.Text = "c";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -218,10 +210,9 @@ namespace Juego_2_grupo3_parejas
             this.label7.AutoSize = true;
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(360, 158);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(271, 129);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(167, 154);
+            this.label7.Size = new System.Drawing.Size(125, 125);
             this.label7.TabIndex = 7;
             this.label7.Text = "c";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -232,10 +223,9 @@ namespace Juego_2_grupo3_parejas
             this.label6.AutoSize = true;
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(183, 158);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(138, 129);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(167, 154);
+            this.label6.Size = new System.Drawing.Size(125, 125);
             this.label6.TabIndex = 6;
             this.label6.Text = "c";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -246,10 +236,9 @@ namespace Juego_2_grupo3_parejas
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(6, 158);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(5, 129);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(167, 154);
+            this.label5.Size = new System.Drawing.Size(125, 125);
             this.label5.TabIndex = 5;
             this.label5.Text = "c";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -260,10 +249,9 @@ namespace Juego_2_grupo3_parejas
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(537, 2);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(404, 2);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(169, 154);
+            this.label4.Size = new System.Drawing.Size(125, 125);
             this.label4.TabIndex = 4;
             this.label4.Text = "c";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -274,10 +262,9 @@ namespace Juego_2_grupo3_parejas
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(360, 2);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(271, 2);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(167, 154);
+            this.label3.Size = new System.Drawing.Size(125, 125);
             this.label3.TabIndex = 3;
             this.label3.Text = "c";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -288,10 +275,9 @@ namespace Juego_2_grupo3_parejas
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(183, 2);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(138, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 154);
+            this.label1.Size = new System.Drawing.Size(125, 125);
             this.label1.TabIndex = 2;
             this.label1.Text = "c";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -302,24 +288,26 @@ namespace Juego_2_grupo3_parejas
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(6, 2);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(5, 2);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(167, 154);
+            this.label2.Size = new System.Drawing.Size(125, 125);
             this.label2.TabIndex = 1;
             this.label2.Text = "c";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label2.Click += new System.EventHandler(this.label_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(712, 629);
+            this.ClientSize = new System.Drawing.Size(534, 511);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Matching Game";
             this.Click += new System.EventHandler(this.label_Click);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -346,6 +334,7 @@ namespace Juego_2_grupo3_parejas
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
